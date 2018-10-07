fcfec74ed8abf376203388b4e51a33c97a6994e8
Anay Wadhera <anay1018@gmail.com>
2018-10-06 18:27:13 -0700

    mata: DU-ify

--- du.mk
+++ du.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -18,9 +18,12 @@ else
 TARGET_RECOVERY_FSTAB := device/essential/mata/rootdir/etc/fstab.mata
 endif
 
+# GApps
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+
 # Device identifiers
 PRODUCT_DEVICE := mata
-PRODUCT_NAME := aosip_mata
+PRODUCT_NAME := du_mata
 PRODUCT_BRAND := essential
 PRODUCT_MODEL := PH-1
 PRODUCT_MANUFACTURER := Essential Products
@@ -29,10 +32,5 @@ PRODUCT_RELEASE_NAME := mata
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=mata \
         PRIVATE_BUILD_DESC="mata-user 9 PPR1.180610.091 249 release-keys"
-
-
-# AICP Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="Vlad Adumitroaie (celtare21)"
 
 BUILD_FINGERPRINT := essential/mata/mata:9/PPR1.180610.091/249:user/release-keys
